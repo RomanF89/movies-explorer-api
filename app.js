678a06a6f2e8a59371c130ca848ba3b92c12c525
Roman Faritov <romko_1992@mail.ru>
2022-08-07 17:20:49 +0500

    feat: изменена валидация

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const { PORT = 3000 } = process.env;
 app.use(cors({
   credentials: true,
   origin:
-  ['https://localhost:3001', 'https://localhost:3000',
+  ['https://localhost:3001', 'https://localhost:3000', 'http://localhost:3001', 'http://localhost:3000',
     'https://movies89api.nomorepartiesxyz.ru', 'http://movies89api.nomorepartiesxyz.ru',
   ],
 }));
